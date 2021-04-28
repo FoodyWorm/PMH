0c774fd2c74b2886765ceda532a0c751a2f6fa55
jangenkim <jangenkim@naver.com>
2021-04-28 16:57:55 +0900

    2021.04.28. WorkSpace(0.2)

--- Backend/app.js
+++ Backend/app.js
@@ -13,6 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('connect-history-api-fallback')());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
